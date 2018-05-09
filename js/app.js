521f0e9dbf170e690d58654a13064c2db2a41c65
Eric M Johnston <emjohn612@gmail.com>
2018-05-09 04:05:55 -0400

    feat: add list of all cats

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
+var initialCats = [
+{
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
+    nickNames: ["Kitty", "Pookie", "Lazy bud"]
+},
+{
+    clickCount : 0,
+    name : 'Tiger',
+    imgSrc : '4154543904_6e2428c421_z.jpg',
+    imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+    nickNames: ["Tigger"]
+},
+{
+    clickCount : 0,
+    name : 'Scaredy',
+    imgSrc : '22252709_010df3379e_z.jpg',
+    imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+    nickNames: ["Casper"]
+},
+{
+    clickCount : 0,
+    name : 'Shadow',
+    imgSrc : '1413379559_412a540d29_z.jpg',
+    imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+    nickNames: ["Shooby"]
+},
+{
+    clickCount : 0,
+    name : 'Sleepy',
+    imgSrc : '9648464288_2516b35537_z.jpg',
+    imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+    nickNames: ["Zzzzz"]
+}
+];
+
 var Cat = function(data){
   this.clickCount = ko.observable(data.clickCount);
   this.name = ko.observable(data.name);
@@ -26,23 +64,23 @@ var Cat = function(data){
           level = "Adult";
           return level;
       };
-  }, this);
+  },this);
 }
 
 var ViewModel = function() {
+    var self = this;
 
-    this.currentCat = ko.observable( new Cat({
-        clickCount: 0,
-        name: 'Tabby',
-        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-        imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
-        nickNames: ["Kitty", "Pookie", "Lazy bud"]
-    }) );
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem){
+        self.catList.push( new Cat(catItem) );
+    })
+
+    this.currentCat = ko.observable( this.catList()[0] );
 
     this.incrementCounter = function(){
         this.clickCount(this.clickCount() + 1);
     };
-
 };
 
 ko.applyBindings(new ViewModel());
